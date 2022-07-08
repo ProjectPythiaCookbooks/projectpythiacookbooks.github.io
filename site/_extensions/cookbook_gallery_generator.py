d4e04730aaf68e38cafaaeeeb41ef372ce063cc1
Julia Kent <46687291+jukent@users.noreply.github.com>
2022-07-08 09:49:14 -0600

    use subtext file

--- site/_extensions/cookbook_gallery_generator.py
+++ site/_extensions/cookbook_gallery_generator.py
@@ -1,4 +1,5 @@
 import yaml
+from itertools import chain
 from gallery_generator import build_from_repos, generate_menu, generate_repo_dicts
 
 
@@ -15,6 +16,11 @@ def main(app):
     stext2 = "Cookbooks are created from Jupyter Notebooks that we strive to binderize so each Cookbook can be executed in the cloud with a single click from your browswer. See documentation [here](https://foundations.projectpythia.org/preamble/how-to-use.html#interacting-with-jupyter-notebooks-in-the-cloud-via-binder) for details. In some instances executing a Cookbook will require downloading the Notebook to your local laptop or desktop as described [here](https://foundations.projectpythia.org/preamble/how-to-use.html#interacting-with-jupyter-books-locally)."
     stext3 = "Interested in contributing a new Cookbook or contributing to an existing Cookbook? Great! Please see the [Project Pythia Contributor's Guide](https://projectpythia.org/contributing.html) and Cookbook-specific information [here](https://github.com/ProjectPythiaCookbooks/.github/blob/main/CONTRIBUTING.md)."
     subtext = stext1 + '<br><br>' + stext2 + '<br><br>' + stext3
+
+    subtext = ''
+    with open('cookbook_gallery_subtext.txt') as fid:
+        for line in fid:
+            subtext = subtext + line
 
     submit_btn_link = 'https://github.com/ProjectPythiaCookbooks/projectpythiacookbooks.github.io/issues/new?assignees=ProjectPythiaCookbooks%2Feducation&labels=content%2Ccookbook-gallery-submission&template=update-cookbook-gallery.yaml&title=Update+Gallery+with+new+Cookbook'
     submit_btn_txt = 'Submit a new Cookbook'
